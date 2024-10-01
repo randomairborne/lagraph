173a7b63dfdef46a60e919443544f9e5faae7c46
valkyrie_pilot <valk@randomairborne.dev>
2024-09-30 21:01:25 -0600

    Prettier

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import {dotfile} from "@randomairborne/rspkg";
-import {instance} from "@viz-js/viz";
+import { dotfile } from "@randomairborne/rspkg";
+import { instance } from "@viz-js/viz";
 
 const df = document.getElementById("ta-x");
 const svg = document.getElementById("svg-p");
 
 function render() {
-    const dot = dotfile(df.value);
-    instance().then((viz) => {
-        while (svg.hasChildNodes()) {
-            svg.removeChild(svg.lastChild);
-        }
-        const new_svg = viz.renderSVGElement(dot);
-        svg.appendChild(new_svg);
-    });
+  const dot = dotfile(df.value);
+  instance().then((viz) => {
+    while (svg.hasChildNodes()) {
+      svg.removeChild(svg.lastChild);
+    }
+    const new_svg = viz.renderSVGElement(dot);
+    svg.appendChild(new_svg);
+  });
 }
 
 df.addEventListener("input", render);
